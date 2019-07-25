9548e34a6c40844670c32255f61e838316bbd756
Unknown <zack.c.campbell@gmail.com>
2019-07-25 18:57:41 -0500

    Updated readme with necessary dependencies and vm options

--- src/Server/MailThread.java
+++ src/Server/MailThread.java
@@ -1,8 +1,6 @@
 package Server;
 
-import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Properties;
 import java.util.Scanner;
 import java.util.logging.Level;
